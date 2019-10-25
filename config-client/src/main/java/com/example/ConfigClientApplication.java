ccbb9d5ac2073f93ef52cb570ab887fa4952ae5c
admin <email@example.com>
2019-10-25 14:57:40 +0800

    initial

--- config-client/src/main/java/com/example/ConfigClientApplication.java
+++ config-client/src/main/java/com/example/ConfigClientApplication.java
@@ -3,17 +3,27 @@ package com.example;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
+@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
     }
+
+    /**
+     * http://localhost:8881/actuator/bus-refresh
+     */
 
     // a.properties a.yml 相同的键显示properties的值
     @Value("${test-version}")
