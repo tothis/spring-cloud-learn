53766d8ed5604caa2d69ab06c784be769bf5cba8
lilei <tothis@qq.com>
2019-11-07 20:34:36 +0800

    initial

--- service/src/main/java/com/example/ServiceApplication.java
+++ service/src/main/java/com/example/ServiceApplication.java
@@ -1,12 +1,17 @@
 package com.example;
 
 import brave.sampler.Sampler;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,20 +19,18 @@ import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @RestController
+@EnableHystrix
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 @EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ServiceApplication {
 
+    // 访问http://localhost:8762/actuator/hystrix.stream
+
     public static void main(String[] args) {
         SpringApplication.run(ServiceApplication.class, args);
-    }
-
-    @Value("${server.port}")
-    private int port;
-
-    @GetMapping
-    public String test() {
-        return "port : " + port;
     }
 
     @Bean
@@ -37,6 +40,19 @@ public class ServiceApplication {
 
     @Autowired
     private RestTemplate restTemplate;
+
+    @Value("${server.port}")
+    private int port;
+
+    @HystrixCommand(fallbackMethod = "error")
+    @GetMapping("test")
+    public String test() {
+        return "port : " + port;
+    }
+
+    private String error() {
+        return "error error error";
+    }
 
     @GetMapping("test1")
     public String test1() {
