0b4cab0fa8e3eb6047096bc4dca67ee3d89739e9
lilei <tothis@qq.com>
2019-11-25 12:11:52 +0800

    initial

--- ribbon-service/src/main/java/com/example/RibbonServiceApplication.java
+++ ribbon-service/src/main/java/com/example/RibbonServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
+// @EnableTransactionManagement // 启注解事务管理 等同于xml配置方式的 <tx:annotation-driven/>
 @EnableHystrix
 @EnableEurekaClient
 @EnableDiscoveryClient // 向服务中心注册
